6921befa0f3d6d86b904b64040b886afeb15e0d7
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-11-01 17:57:30 +0100

    sign in/SIGN UP with appwrite

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -42,7 +42,8 @@ userData;
 export async function getLoggedInUser () {
     try {
         const { account }= await createSessionClient();
-        return await account.get();
+        const user = await account.get();
+        return parseStringify(user);
     }   catch (error) {
         return null; 
     } 
