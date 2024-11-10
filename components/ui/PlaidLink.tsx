552663d700f0c8123c4b168366f6c18a262e1eb3
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-11-10 23:06:05 +0100

    Plaid Button

--- components/ui/PlaidLink.tsx
+++ components/ui/PlaidLink.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
 import { Button } from './button'
+import { StyledString } from 'next/dist/build/swc';
 
-const PlaidLink = ({user, variant}:PlaidLinkProps) => {
+const PlaidLink = ({user, variant}: PlaidLinkProps) => {
+    const [token, setToken] = useState('');
+    useEffect(() => {
+        const getLinkToken = async () => {
+            // const data =  await createLinkToken(user);
+            // setToken(data?.linkToken)
+        }
+        getLinkToken();
+    }
+    )
+
+    const onSuccess = useCallback(async (public_token: 
+        StyledString) => {
+            // await exchangePublicToken({
+            //     publicToken: public_token,
+            // })
+
+    }, [user])
+
+    const config: PlaidLinkOptions ={
+        token,
+        onSuccess
+    }
+
   return (
    <>
    {
     variant ==='primary' ?(
-        <Button>
-            Connect Bank
+        <Button
+        className="plaidlink-primary">
+            Connect bank
         </Button>
     ): variant === 'ghost' ?(
         <Button>
