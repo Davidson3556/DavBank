6f856eb04d455e4a0ddd2e5b377f39efe0c91946
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-12-05 23:54:21 +0100

    success

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,49 +1,46 @@
-import HeaderBox from '@/components/ui/HeaderBox'
-import RightSidebar from '@/components/ui/RightSidebar';
-import TotalBalanceBox from '@/components/ui/TotalBalanceBox';
-import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
-import { getLoggedInUser } from '@/lib/actions/user.actions';
+import HeaderBox from "@/components/ui/HeaderBox";
+import RightSidebar from "@/components/ui/RightSidebar";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import { getAccount, getAccounts } from "@/lib/actions/bank.actions";
+import { getLoggedInUser } from "@/lib/actions/user.actions";
 
-const Home = async({searchParams: { id, page } }: SearchParamProps ) => {
-  const loggedIn= await getLoggedInUser();
-  const accounts = await getAccounts({ userId: loggedIn.$userId });
+const Home = async ({ searchParams: { id } }: SearchParamProps) => {
+  const loggedIn = await getLoggedInUser();
+  const accounts = await getAccounts({ userId: loggedIn.$id });
 
-  if(!accounts) return;
+  if (!accounts) return;
 
   const accountsData = accounts?.data;
 
   const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
 
-  const account = await getAccount({appwriteItemId});
+  const account = await getAccount({ appwriteItemId });
 
   return (
     <section className="home">
-    <div 
-    className="home-content">
-      <header 
-      className="home-header">
-            <HeaderBox
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
             type="greeting"
             title="Welcome"
-            user={loggedIn?.name || 'Guest'}
+            user={loggedIn?.firstName || "Guest"}
             subtext="Easily access and manage your account and transactions."
-            />
-            <TotalBalanceBox
+          />
+          <TotalBalanceBox
             accounts={accountsData}
             totalBanks={accounts?.totalBanks}
-            totalCurrentBalance={account?.currentBalance}
-            />
-
-      </header>
-      RECENT TRANSACTIONS
-    </div>
-    <RightSidebar
-    user={loggedIn}
-    transactions={accounts?.transactions}
-    banks={ accountsData?.slice(0,2)}
-    />
+            totalCurrentBalance={accounts?.totalCurrentBalancecurrent}
+          />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar
+        user={loggedIn}
+        transactions={accounts?.transactions}
+        banks={accountsData?.slice(0, 2)}
+      />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
