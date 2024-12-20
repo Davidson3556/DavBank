83463f2d973b59651a97dd722e503393c91dfd7b
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-12-20 14:38:28 +0100

    css fixes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -8,16 +8,21 @@ export default function RootLayout({
     return (
       <main className="flex min-h-screen w-full
       justify-between font-inter bg-[#41005C]">
-          
           <div className="auth-asset">
            
             <div>
+           <p className="text-[#8A00C2] font-bold ml-8 mt-8">TRANSACTLY</p>
                 <Image
                 src="/icons/login.svg"
                 alt="Login Image"
                 width={750}
                 height={500}
-                />
+                className="self-center h-[500px]"
+                                />
+
+          <p className="text-[#8A00C2] font-bold justify-items-center grid text-2xl">Simplifying
+           Payments, <br/>Reshaping Transactions</p>
+
             </div>
 
          
