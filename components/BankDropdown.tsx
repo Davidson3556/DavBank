7041052e4d6bf572f278e648acbce8b553268a0b
Davidson3556 <99369614+Davidson3556@users.noreply.github.com>
2024-12-23 01:02:33 +0100

    tranfer_fund ui fixes

--- components/BankDropdown.tsx
+++ components/BankDropdown.tsx
@@ -48,7 +48,7 @@ export const BankDropdown = ({
         className={`flex w-full bg-white gap-3 md:w-[300px] ${otherStyles}`}
       >
         <Image
-          src="icons/credit-card.svg"
+          src="icons/credit-card2.svg"
           width={20}
           height={20}
           alt="account"
